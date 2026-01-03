b266e2093dcbfa9d34ba4b1add70dde039d13729
Raman <ramantiwary19@gmail.com>
2026-01-03 18:39:32 +0530

    change in server file

--- backend/server.js
+++ backend/server.js
@@ -11,5 +11,6 @@ app.use(express.json());
 
 
 app.use("/api/contacts", require("./routes/contactRoutes"));
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => console.log("Server running on port 5000"));
